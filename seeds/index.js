6162fd4c824d30a42d9c89a6b2fc4a37ea6536c4
Paulina <paulinasaldarriaga@gmail.com>
2023-08-04 01:38:23 -0500

    Diseño básico para la página

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 20);
+        const price = Math.floor(Math.random()* 20)+15;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(places)} ${sample(descriptors)}`
+            title: `${sample(places)} ${sample(descriptors)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description:' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde deserunt rem minus tempora minima quos, incidunt aperiam corrupti eaque! Obcaecati, quasi earum! Quisquam dolorum aut minus laudantium totam porro eos.',
+            price: price
         })
         await camp.save();
     }
@@ -38,3 +42,4 @@ seedDB().then(() => {
 seedDB().then(() =>{
     mongoose.connection.close();
 })
+
